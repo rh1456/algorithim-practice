077f5a29484fd3fd642be8a6cb68547d15fce88e
Reid Hanson <reidhans@buffalo.edu>
2020-03-10 19:53:25 -0400

    ransomNote Algorithim

--- public/harmlessRansom.js
+++ public/harmlessRansom.js
@@ -1,13 +1,3 @@
-// // fizz buzz practice//
-// function fizzBuzz(num) {
-//   for (var i = 1; i <= num; i++) {
-//     if (i % 15 === 0) console.log('FizzBuzz')
-//     else if (i % 3 === 0) console.log('Fizz')
-//     else if (i % 5 === 0) console.log('Buzz')
-//     else console.log(i)
-//   }
-// }
-// fizzBuzz(30)
 function harmlessRansomNote(noteText, magazineText) {
   var noteArr = noteText.split(' ')
   var magazineArr = magazineText.split('')
